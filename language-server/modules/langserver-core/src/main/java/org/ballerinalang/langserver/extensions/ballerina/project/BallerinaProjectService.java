ballerina-platform/ballerina-lang

Add language server extension "ballerinaProject"
This extension handles requests related to a whole ballerina project.
Currently the only request is "modules" which returns ASTs of all the modules the project contains.

language-server/modules/langserver-core/src/main/java/org/ballerinalang/langserver/extensions/ballerina/project/BallerinaProjectService.java
@@ -1,0 +1,19 @@
+package org.ballerinalang.langserver.extensions.ballerina.project;
+
+import org.ballerinalang.langserver.extensions.ballerina.document.BallerinaASTRequest;
+import org.ballerinalang.langserver.extensions.ballerina.document.BallerinaASTResponse;
+import org.eclipse.lsp4j.jsonrpc.services.JsonRequest;
+import org.eclipse.lsp4j.jsonrpc.services.JsonSegment;
+
+import java.util.concurrent.CompletableFuture;
+
+/**
+ * An extension interface for Language server to add features related to ballerina files.
+ *
+ * @since 1.0.0
+ */
+@JsonSegment("ballerinaProject")
+public interface BallerinaProjectService {
+    @JsonRequest
+    CompletableFuture<BallerinaASTResponse> modules(BallerinaASTRequest request);
+}

roonyh/ballerina

language-server/modules/langserver-core/src/main/java/org/ballerinalang/langserver/extensions/ballerina/project/BallerinaProjectService.java
@@ -1,0 +1,19 @@
+package org.ballerinalang.langserver.extensions.ballerina.project;
+
+import org.ballerinalang.langserver.extensions.ballerina.document.BallerinaASTRequest;
+import org.ballerinalang.langserver.extensions.ballerina.document.BallerinaASTResponse;
+import org.eclipse.lsp4j.jsonrpc.services.JsonRequest;
+import org.eclipse.lsp4j.jsonrpc.services.JsonSegment;
+
+import java.util.concurrent.CompletableFuture;
+
+/**
+ * An extension interface for Language server to add features related to ballerina files.
+ *
+ * @since 1.0.0
+ */
+@JsonSegment("ballerinaProject")
+public interface BallerinaProjectService {
+    @JsonRequest
+    CompletableFuture<ModulesResponse> modules(ModulesRequest request);
+}